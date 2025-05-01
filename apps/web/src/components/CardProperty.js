81a6a371aff9e776aabd9b611d56488da157f264
Ailen Franco <ailenfranco43@gmail.com>
2025-05-01 16:43:58 -0300

    feat(properties): enhance property cards with tags and styling

--- apps/web/src/components/CardProperty.js
+++ apps/web/src/components/CardProperty.js
@@ -1,5 +1,5 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export const CardProperty = ({ property }) => {
   const {
@@ -11,26 +11,17 @@ export const CardProperty = ({ property }) => {
     createdAt,
     updatedAt,
     id,
-  } = property
+    max_people,
+  } = property;
 
-  //   {
-  //     "title": "Hermosa casa de campo",
-  //     "description": "Una encantadora casa de campo rodeada de naturaleza, ideal para escapadas familiares.",
-  //     "price": 17666.45,
-  //     "photos": [
-  //         "url_de_foto_1.jpg",
-  //         "url_de_foto_2.jpg",
-  //         "url_de_foto_3.jpg"
-  //     ],
-  //     "tags": [],
-  //     "createdAt": "2024-10-17T16:19:17.265Z",
-  //     "updatedAt": "2024-10-17T16:19:17.265Z",
-  //     "id": "671139059bc699366d668548"
-  // },
+  const truncateText = (text, maxLength) => {
+    return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+  };
 
-  const photoUrl = photos.length > 0
-    ? photos[0]
-    : 'https://agentrealestateschools.com/wp-content/uploads/2021/11/real-estate-property.jpg'
+  const photoUrl =
+    photos.length > 0
+      ? photos[0]
+      : "https://agentrealestateschools.com/wp-content/uploads/2021/11/real-estate-property.jpg";
 
   return (
     <div className="border-2 border-[#5FA77780] rounded-2xl overflow-hidden shadow-lg transition-transform transform hover:scale-105 hover:cursor-pointer">
@@ -42,21 +33,41 @@ export const CardProperty = ({ property }) => {
           alt={`Photo of property`}
           className="object-cover w-full h-auto"
         />
+          <div className="w-full p-2 flex flex-wrap gap-1">
+            {tags &&
+              tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="bg-[rgba(95,167,119,.6)] text-white text-xs font-semibold px-2 py-1 rounded-full "
+                >
+                  {tag}
+                </span>
+              ))}
+          </div>
 
-        <div className='p-4'>
-          <h3 className="font-roboto font-bold">{title}</h3>
+        <div className="p-4 pt-0">
+          <h3 className="font-roboto font-bold capitalize">{title}</h3>
+          <div>
+            <p className="font-roboto text-sm text-gray-700 mt-2">
+              {truncateText(description, 100)}{" "}
+              {/* ajustá el número a tu preferencia */}
+            </p>
+          </div>
 
-          <div className='ml-1'>
-            <p className="font-roboto text-sm font-bold text-slate-600">5 Huéspedes</p>
-            <p className="font-roboto text-sm font-bold text-slate-600">4 Camas</p>
 
-            <p className="font-roboto font-bold">
+          <div className="ml-1 py-2">
+            <p className="font-roboto text-sm font-bold text-slate-600">
+              {" "}
+              Capacidad: {max_people} personas
+            </p>
+
+            <p className="font-roboto font-bold pt-1">
               <span className="text-[#5FA777]">${price}</span>
               <span className="text-black">/noche</span>
             </p>
           </div>
-        </div>      
+        </div>
       </Link>
     </div>
-  )
-}
+  );
+};
