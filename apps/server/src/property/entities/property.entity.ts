2040c2f4c8db6c29522d2846ee28c91e829a9f4a
Ailen Franco <ailenfranco43@gmail.com>
2025-11-10 20:44:26 -0300

    fix: build errors for Render deploy

--- apps/server/src/property/entities/property.entity.ts
+++ apps/server/src/property/entities/property.entity.ts
@@ -5,7 +5,7 @@ import { Document, Types } from 'mongoose';
 @Schema({
   timestamps: true,
   toJSON: {
-    transform: function (doc, ret) {
+    transform: function (doc, ret:any) {
       ret.id = ret._id;
       delete ret._id;
       delete ret.__v;
